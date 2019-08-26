52e891d2a8a2f8356e81ec40c62395061113a18f
App Academy Student <aa@Apps-Mac-mini-37.local>
2019-08-26 18:09:51 -0400

    End of day work

--- lib/04a_countries_bonus.rb
+++ lib/04a_countries_bonus.rb
@@ -17,13 +17,42 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    countries
+  WHERE
+    gdp > ALL (
+      SELECT
+        gdp
+      FROM
+        countries
+      WHERE
+        continent = 'Europe' 
+        AND gdp IS NOT NULL 
+    )
   SQL
 end
 
 def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
-  execute(<<-SQL)
+  execute(<<-SQL)  
+  SELECT
+    c1.continent,
+    c1.name, 
+    c1.area 
+  FROM
+    countries AS c1 
+  WHERE
+    c1.area = (
+      SELECT 
+        MAX (c2.area)
+      FROM 
+        countries AS c2 
+      WHERE 
+        c1.continent = c2.continent 
+    )
   SQL
 end
 
